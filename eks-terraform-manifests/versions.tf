26b0d42efac7fd34fcfe57bb02b3eb052071a235
Aliakber Kachwala <ali.kachwala@gmail.com>
2026-02-06 21:58:48 +0000

    eks terraform manifest files added

---
+++ eks-terraform-manifests/versions.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+  }
+  # Remote Backend
+  backend "s3" {
+    bucket  = "aliakber-terraform-state-backend"
+    key     = "eks/dev/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
